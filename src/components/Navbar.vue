0d9379a5e309134e2bca454b2a400de628289b9d
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-07 20:50:53 +0800

    style: modify header & navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
-  <nav class="navbar">
-    <router-link class="navbar-brand" to="/">
-      <div class="navbar-brand-title">
-        趣台北<font-awesome-icon icon="map-signs"></font-awesome-icon>
-      </div>
-    </router-link>
-  </nav>
+  <header>
+    <nav class="navbar">
+      <router-link class="navbar-brand" to="/">
+        <div class="navbar-brand-title">
+          <div class="navbar-brand-title-text">趣台北</div>
+          <font-awesome-icon
+            icon="map-signs"
+            class="navbar-brand-title-icon"
+          ></font-awesome-icon>
+        </div>
+      </router-link>
+    </nav>
+  </header>
 </template>
