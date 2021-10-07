ac820537168686588fb317579265994a80edf289
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-07 17:58:39 +0800

    feat: add map-signs icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar">
     <router-link class="navbar-brand" to="/">
-      <div class="navbar-brand-title">趣台北</div>
+      <div class="navbar-brand-title">
+        趣台北<font-awesome-icon icon="map-signs"></font-awesome-icon>
+      </div>
     </router-link>
   </nav>
 </template>
