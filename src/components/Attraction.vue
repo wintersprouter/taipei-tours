a24cbcb7b23a0071e33015f4d8a169dcc5a727a2
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-08 15:06:08 +0800

    feat: add Attraction components

---
+++ src/components/Attraction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card">
+    <div class="card-cover">
+      <img src="" alt="attraction-card" class="card-cover-img" />
+    </div>
+    <div class="card-content">
+      <div class="card-content-title">Lorem ipsum dolor</div>
+      <div class="card-content-text"></div>
+      <button class="card-content-button">More</button>
+    </div>
+  </div>
+</template>
---
+++ src/view/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <section><Attraction /></section>
+</template>
+<script>
+import Attraction from "../components/Attraction.vue";
+export default {
+  components: { Attraction },
+};
+</script>
