6d30bb996dd825a3daf30f5d10fb3220c3537e7d
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-08 15:11:31 +0800

    feat: add vue router

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { faMapSigns } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 library.add(faMapSigns)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
