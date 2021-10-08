918492cb8884b9d241fc83b52000326a53e06eeb
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-08 15:40:43 +0800

    feat: fetch Attractions api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,23 @@
 </template>
 <script>
 import Attraction from "../components/Attraction.vue";
+import attractionsAPI from "./../apis/attractions";
 export default {
+  name: "Home",
   components: { Attraction },
+  created() {
+    this.fetchAttractions();
+  },
+  methods: {
+    async fetchAttractions() {
+      try {
+        const response = await attractionsAPI.getAttractions();
+        console.log(response.data);
+      } catch (error) {
+        this.isLoading = false;
+        console.log("error", error);
+      }
+    },
+  },
 };
 </script>
