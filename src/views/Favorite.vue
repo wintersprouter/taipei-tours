33f4b46063ae4729b3b919e5cf46db9fc2a6be39
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-10-12 15:52:29 +0800

    feat: add favorite pagination

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -6,45 +6,84 @@
       <div class="container">
         <div class="attractions-wrapper">
           <Attraction
-            v-for="favorAttraction in favorAttractions"
-            :key="favorAttraction.id"
-            :initial-attraction="favorAttraction"
+            v-for="attraction in attractions"
+            :key="attraction.id"
+            :initial-attraction="attraction"
             @after-add-favorite="afterAddFavorite"
             @after-remove-favorite="afterAddFavorite"
           />
         </div>
       </div>
     </section>
+    <footer>
+      <Pagination
+        v-if="totalPage.length > 1"
+        :current-page="currentPage"
+        :pages="pages"
+        :total-page="totalPage"
+        :previous-page="previousPage"
+        :next-page="nextPage"
+        :is-favorited-list="isFavoritedList"
+        @after-click-page="fetchFavoriteAttractions"
+      />
+    </footer>
   </div>
 </template>
 <script>
 import NavTabs from "../components/NavTabs.vue";
 import Attraction from "../components/Attraction.vue";
+import Pagination from "../components/Pagination.vue";
 const STORAGE_KEY = "favorite-list";
 export default {
   name: "Favorite",
-  components: { NavTabs, Attraction },
+  components: { NavTabs, Attraction, Pagination },
   data() {
     return {
       favorAttractions: [],
+      attractions: [],
       isEmpty: false,
+      pages: 1,
+      totalPage: [],
+      currentPage: 1,
+      isFavoritedList: true,
     };
   },
   created() {
     this.fetchFavoriteAttractions();
   },
   methods: {
-    async fetchFavoriteAttractions() {
+    async fetchFavoriteAttractions(page) {
       this.favorAttractions =
         JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
       if (!this.favorAttractions.length) {
         this.isEmpty = true;
       }
+      this.currentPage = page ? page : 1;
+      this.attractions = this.getAttractionsByPage(this.currentPage);
+      this.generatePagination();
     },
+    generatePagination() {
+      const pageLimit = 30;
+      const totalpages = [];
+      const total = this.favorAttractions.length;
+      const pages = Math.ceil(total / pageLimit);
+      for (let i = 1; i <= pages; i++) {
+        totalpages.push(i);
+      }
+      this.pages = pages;
+      this.totalPage = totalpages;
+    },
+    getAttractionsByPage(page) {
+      const pageLimit = 30;
+      const startIndex = (page - 1) * pageLimit;
+      return this.favorAttractions.slice(startIndex, startIndex + pageLimit);
+    },
+
     afterAddFavorite(favoritedAttraction) {
       const { id, name } = favoritedAttraction;
       const isFavorited = this.favorAttractions.some((data) => data.id === id);
       if (isFavorited) {
+        this.attractions = this.attractions.filter((data) => data.id !== id);
         this.favorAttractions = this.favorAttractions.filter(
           (data) => data.id !== id
         );
